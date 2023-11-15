a30b4366b26a958964c2f8469053e5a6d00fbff1
spandanakoppaku <skoppaku@gmail.com>
2023-11-15 15:42:58 -0500

    Auto Push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -1,8 +1,6 @@
 module "cart" {
-source                 = "./vendor/modules/docdb" 
+source                 = "./vendor/modules/app" 
 ENV                    = var.ENV
-DOCDB_INSTANCE_TYPE    = var.DOCDB_INSTANCE_TYPE
-DOCDB_INSTANCE_COUNT   = var.DOCDB_INSTANCE_COUNT
-DOCDB_PORT_NUMBER      = var.DOCDB_PORT_NUMBER
+COMPONENT              = var.COMPONENT
 
 }
